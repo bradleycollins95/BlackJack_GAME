ea73990f0f356813cc788a2a90eded9bcd4fa6da
Bradley Collins <113688789+bradleycollins95@users.noreply.github.com>
2022-12-08 01:52:00 -0330

    fixed code bugs

--- BLACKJACK_GAME_WORKING/src/BlackjackGame.java
+++ BLACKJACK_GAME_WORKING/src/BlackjackGame.java
@@ -8,9 +8,6 @@ public class BlackjackGame {
         ArrayList<PlayingCard> deck = new ArrayList<>();
         CardDeck cardDeck = new CardDeck(deck);
 
-        //Scanner scanner = new Scanner(System.in);
-
-        //int wager = Integer.parseInt(scanner.nextLine());
         int score = 0;
         int gameCounter = 1;
 
@@ -58,26 +55,24 @@ public class BlackjackGame {
         while(playerHandValue < 21){
             System.out.println("Hit or stay? (h/s)\n");
             String choice = sc.nextLine();
-            if(choice.equalsIgnoreCase("h")){
-                //hit - deal a card to player
+            if(choice.equalsIgnoreCase("h")){ //hit - deal a card to player
                 playerHand.add(cardDeck.drawCards());
-                //display updated player hand and value
-                System.out.println("Dealer hand: " + dealerHand);
-                System.out.println("Dealer Hand Value: " + dealerHandValue);
+                System.out.println("Dealer hand: " + dealerHand); //display updated player hand and value
+                System.out.println("Dealer Hand Value: " + dealerHandValue); //display updated player hand and value
                 System.out.println();
                 System.out.println("Player Hand: " + playerHand);
                 playerHandValue = calculateHandValue(playerHand);
                 System.out.println("Player Hand Value: " + playerHandValue);
             } else if(choice.equalsIgnoreCase("s")){
                 while(dealerHandValue < playerHandValue){
+                    System.out.println("Dealer draws another card.\n");
                     dealerHand.add(cardDeck.drawCards());
                     System.out.println("Dealer hand: " + dealerHand);
                     dealerHandValue = calculateHandValue(dealerHand);
                     System.out.println("Dealer hand value: " + dealerHandValue);
                     System.out.println();
                 }
-                //stay - break out of loop
-                break;
+                break; //stay; exit loop
             }
         }
 
@@ -85,26 +80,21 @@ public class BlackjackGame {
         if(playerHandValue == 21 && dealerHandValue == 21){
             //if both hands have blackjack, it's a tie
             System.out.println("Tie!");
-        } else if(playerHandValue == 21){
-            //if player has blackjack, player wins
+        } else if(playerHandValue == 21){ //if player has blackjack, player wins
             System.out.println("Player wins!");
             score++;
-        } else if(dealerHandValue == 21){
-            //if dealer has blackjack, dealer wins
+        } else if(dealerHandValue == 21){ //if dealer has blackjack, dealer wins
             System.out.println("Dealer wins!");
-        } else if(dealerHandValue > 21){
-            //if dealer busts, player wins
+        } else if(dealerHandValue > 21){ //if dealer busts, player wins
             System.out.println("Dealer busts, player wins!");
             score++;
-        } else{
-            //if no blackjacks, compare hand values
+        } else{ //if no blackjacks, compare hand values
             if(playerHandValue > dealerHandValue){
                 //if the player's hand exceeds the dealers hand
                 //and is higher than 21, they lose
                 if(playerHandValue > 21){
                     System.out.println("Player busts, dealer wins!");
-                } else{
-                    //otherwise the player wins
+                } else{ //otherwise the player wins
                     score++;
                     System.out.println("Player wins!");
                 }
@@ -114,8 +104,7 @@ public class BlackjackGame {
                 if(dealerHandValue > 21){
                     System.out.println("Dealer busts, player wins!");
                     score++;
-                } else{
-                    //if both hands have the same value, tie
+                } else{ //if both hands have the same value, tie
                     System.out.println("Tie!");
                 }
             }
@@ -130,13 +119,11 @@ public class BlackjackGame {
         //ask if player wants to play again
         System.out.println("Play again? (y/n)\n");
         String playAgain = sc.nextLine();
-        if(playAgain.equalsIgnoreCase("y")){
-            //restart game
-            main(args);
-        } else if(playAgain.equalsIgnoreCase("n")){
-            //end game
+        if(playAgain.equalsIgnoreCase("y")){ //restart game
+            main(args);                                 //restart game
+        } else if(playAgain.equalsIgnoreCase("n")){ //end game
             System.out.println("Goodbye!");
-            System.exit(0);
+            System.exit(0); //end game
         }
     }
 
